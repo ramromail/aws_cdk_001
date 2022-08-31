53b061756871adae84c23d951fbc5ae1b5c6ea99
Ranjeet Kumar <ranjeet.kumar@admicom.fi>
2022-08-31 19:10:26 +0300

    Initial commit

---
+++ test/lmda_s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LmdaS3 from '../lib/lmda_s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lmda_s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LmdaS3.LmdaS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
